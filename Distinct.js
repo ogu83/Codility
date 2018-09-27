9951e21ab4d04c6abe6e846bbd2da91544681e5d
OGUZ <korogu@hotmail.com>
2018-09-27 08:53:58 +0300

    Create Distinct.js

---
+++ Distinct.js
@@ -0,0 +1,9 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var result = Array.from(new Set(A));
+    //console.log(result);
+    return result.length;
+}
